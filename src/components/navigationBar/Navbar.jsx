02bc2b78a256fb63978733d320c75193246f1fad
Riadul Islam <riadul551@gmail.com>
2026-06-21 14:43:16 +0600

    feat: update Navbar component to use new logo and improve branding

--- src/components/navigationBar/Navbar.jsx
+++ src/components/navigationBar/Navbar.jsx
@@ -5,20 +5,22 @@ import ThemeToggleButton from "./ThemeToggleButton";
 import MenuToggleIcon from "./MenuToggleIcon";
 import Navlinks from "./Navlinks";
 import SessionToggleButton from "./SessionToggleButton";
+import logo from "../../../public/assets/logo.png";
 
 const Navbar = async () => {
   return (
     <div className="w-full fixed top-0 z-50 bg-white dark:bg-gray-900   shadow-sm shadow-gray-300 dark:shadow-gray-700">
       <div className="container mx-auto  flex items-center justify-between p-4">
-        <div className="flex items-center w-25 h-7.5 ">
+        <div className="flex items-center w-14 ">
           <a href="/">
             <Image
-              src={"/assets/logo.png"}
-              alt="Logo"
+              src={logo}
+              alt="WorkLio logo"
               height={200}
               width={200}
               className="w-full h-full"
             />
+            <span className="font-bold">Work<span className="text-sky-500 dark:text-green-500 font-extrabold">Lio</span></span>
           </a>
         </div>
         <div className="flex items-center">
